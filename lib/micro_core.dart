ce6a01dc740a228c20f5e6b678e5aa4278ed2531
jvictor-antonucci <joao.antonucci@abamais.com>
2023-06-05 21:09:07 -0300

    feat: export files

--- lib/micro_core.dart
+++ lib/micro_core.dart
@@ -1,1 +1,7 @@
 library micro_core;
+
+export 'app/base_app.dart';
+export 'module/module.dart';
+export 'module/traits/traits.dart';
+export 'injection/injection.dart';
+export 'utils/base_app_util.dart';
